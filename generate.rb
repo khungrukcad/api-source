ee71f4d56f74d01bc5e02f0ba3482b3af18bd9a8
Davide De Rosa <keeshux@gmail.com>
2018-12-24 20:15:15 +0100

    Consolidate exception handling

--- generate.rb
+++ generate.rb
@@ -5,12 +5,12 @@ require_relative "lib/notify"
 
 Dotenv.load(".env.default", ".env.secret")
 providers = ENV["PROVIDERS"].split(" ")
-failed = []
+soft_failures = []
 
 web = "gen/#{ENV["VERSION"]}"
-
 endpoint = "net"
-FileUtils.mkdir_p("#{web}/#{endpoint}")
+path = "#{web}/#{endpoint}"
+FileUtils.mkdir_p(path)
 
 providers.each { |name|
     key = name.downcase
@@ -24,32 +24,42 @@ providers.each { |name|
     #rm -f "$WEB/$ENDPOINT/$JSON"
     puts "Scraping..."
 
-    json_string = `sh providers/#{key}/#{endpoint}.sh`
-    if !$?.success?
-        failed << name
-        puts "Failed!"
-        next
+    begin
+        json_string = `sh providers/#{key}/#{endpoint}.sh`
+        raise "#{name}: #{endpoint}.sh failed or is missing" if !$?.success?
+
+        json = nil
+        begin
+            json = JSON.parse(json_string)
+        rescue
+            raise "#{name}: invalid JSON"
+        end
+
+        # inject metadata
+        json["build"] = ENV["MIN_BUILD"].to_i
+        json["name"] = name
+
+        file = File.new("#{path}/#{resource}", "w")
+        file << json.to_json
+        file.close()
+
+        puts "Completed!"
+    rescue StandardError => msg
+
+        # keep going
+        soft_failures << name
+
+        puts "Failed: #{msg}"
     end
-
-    json = JSON.parse(json_string)
-
-    # inject metadata
-    json["build"] = ENV["MIN_BUILD"].to_i
-    json["name"] = name
-
-    file = File.new("#{web}/#{endpoint}/#{resource}", "w")
-    file << json.to_json
-    file.close()
-
-    puts "Completed!"
 }
 
-if !failed.empty?
+# succeed but notify soft failures
+if !soft_failures.empty?
     puts
-    puts "Notifying failed providers: #{failed}"
+    puts "Notifying failed providers: #{soft_failures}"
     notify_failures(
         ENV["TELEGRAM_TOKEN"],
         ENV["TELEGRAM_CHAT"],
-        failed
+        soft_failures
     )
 end
