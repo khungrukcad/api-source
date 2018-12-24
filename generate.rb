dd49db1944a544be0f8a68a54ac5855c91c6582d
Davide De Rosa <keeshux@gmail.com>
2018-12-24 21:27:38 +0100

    Fail script on provider hijacking
    Stop if:
    
    - Any file or submodule is altered before running script.
    - Any generated JSON is improperly altered.
    
    Fixes #1

--- generate.rb
+++ generate.rb
@@ -1,20 +1,29 @@
 require "dotenv"
 require "json"
 require "fileutils"
+require "digest"
 require_relative "lib/notify"
 
 Dotenv.load(".env.default", ".env.secret")
+
 providers = ENV["PROVIDERS"].split(" ")
 soft_failures = []
 
 web = "gen/#{ENV["VERSION"]}"
 endpoint = "net"
 path = "#{web}/#{endpoint}"
+digests = {}
+
 FileUtils.mkdir_p(path)
 
 providers.each { |name|
     key = name.downcase
     resource = "#{key}.json"
+
+    # ensure that repo and submodules were not altered
+    repo_status = `git status --porcelain`
+    puts repo_status
+    raise "Dirty git status" if !repo_status.empty?
 
     puts
     puts "====== #{name} ======"
@@ -39,9 +48,13 @@ providers.each { |name|
         json["build"] = ENV["MIN_BUILD"].to_i
         json["name"] = name
 
+        json_string = json.to_json
         file = File.new("#{path}/#{resource}", "w")
         file << json.to_json
         file.close()
+
+        # save JSON digest
+        digests[key] = Digest::SHA1.hexdigest(json_string)
 
         puts "Completed!"
     rescue StandardError => msg
@@ -51,6 +64,17 @@ providers.each { |name|
 
         puts "Failed: #{msg}"
     end
+}
+
+# fail abruptly on JSON hijacking
+providers.each { |name|
+    next if soft_failures.include? name
+
+    key = name.downcase
+    resource = "#{key}.json"
+    subject = IO.binread("#{path}/#{resource}")
+    md = Digest::SHA1.hexdigest(subject)
+    raise "#{name}: corrupt digest" if md != digests[key]
 }
 
 # succeed but notify soft failures
